665f42d714b149070aa99ed56e88fa1a77f58b53
Jay Shah <jshah200498@gmail.com>
2021-05-03 15:03:42 +0530

    Initial commit

---
+++ lib/CustomUI/custom_card.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:whatsapp_clone/model/chat_model.dart';
+
+class CustomCard extends StatelessWidget {
+  const CustomCard({Key key, this.chatModel, this.sourceChat}) : super(key: key);
+  final ChatModel chatModel;
+  final ChatModel sourceChat;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => IndividualPage(
+                  chatModel: chatModel,
+                  sourceChat: sourceChat,
+                )));
+      },
+      child: Column(
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              radius: 30,
+              child: SvgPicture.asset(
+                chatModel.isGroup ? "assets/groups.svg" : "assets/person.svg",
+                color: Colors.white,
+                height: 36,
+                width: 36,
+              ),
+              backgroundColor: Colors.blueGrey,
+            ),
+            title: Text(
+              chatModel.name,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Row(
+              children: [
+                Icon(Icons.done_all),
+                SizedBox(
+                  width: 3,
+                ),
+                Text(
+                  chatModel.currentMessage,
+                  style: TextStyle(
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+            trailing: Text(chatModel.time),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 20, left: 80),
+            child: Divider(
+              thickness: 1,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
